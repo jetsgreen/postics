e3f753d51b17df3a7ce2ceac522b51c90b4f3aef
Felix Fernandez <fernandezfelix460.gmail.com>
2020-05-07 15:30:19 -0400

    added mobile responsiveness

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -8,8 +8,6 @@ const Jumbotron = () => {
     return (
 
         <React.Fragment>
-            
-             
             <section className="showcase">
                 
                 <div className="video-container">
@@ -17,8 +15,8 @@ const Jumbotron = () => {
 
                 </div>
                 <div className="content">
-                    <h1>Felix Luxury Limo</h1>
-                    <h3>We Can Get You There</h3>
+                    <h2>Felix Luxury Limo</h2>
+                    <h4>We Can Get You There</h4>
                    
                     <NavLink to="/contact" className="btn">Make a Reservation</NavLink>
                     <NavLink to="/about" className="btn">Learn More</NavLink>
